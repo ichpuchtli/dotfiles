d41e71a3f2add57c5eb509ca5c31c81f6630bded
Sam Macpherson <sam.mack91@gmail.com>
2013-02-18 13:05:46 +1000

    Switched monitors around so updated randr settings and conky vertical offset

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-xrandr --newmode "1280x1080" 115.00 1280 1368 1496 1712 1080 1083 1093 1120 -hsync +vsync
-
-xrandr --addmode DVI-I-1 1280x1080
-
-xrandr --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal --output TV-1 --off --output DVI-I-1 --mode 1280x1080 --pos 1920x0 --rotate normal
+xrandr --output VGA-1 --mode 1920x1080 --pos 1280x0 --rotate normal --output TV-1 --off --output DVI-I-1 --mode 1280x1080 --pos 0x0 --rotate normal
